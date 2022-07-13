d549050ab95b40367e01599f0a7b0a450ff56abc
allworldg <944937881@qq.com>
2022-07-13 12:42:08 +0800

    update comment

--- init.lua
+++ init.lua
@@ -2,13 +2,16 @@ require("plugins")
 require("keybindings")
 require("colorscheme")
 require("basic")
+-- lsp
+require('lsp/nvim-cmp')
+require("lsp.lspconfig")
+
+--plugin config
 require("plugin-config.telescope")
 require("plugin-config.nvim-treesiter")
 require("plugin-config.nvim-tree")
 require("plugin-config.bufferline")
-require('lsp/nvim-cmp')
-require("lsp.lspconfig")
-
-
+require("plugin-config.nvim-autopairs")
+require("plugin-config.comment")
 
 
--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -175,6 +175,26 @@ pluginKeys.nvimTreeList = { -- 打开文件或文件夹
   -- 进入上一级
   { key = { "[" }, action = "dir_up" },
 }
+
+--Comment
+-- see ./lua/plugin-config/comment.lua
+pluginKeys.comment = {
+  -- Normal 模式快捷键
+  toggler = {
+    line = "gcc", -- 行注释
+    block = "gbc", -- 块注释
+  },
+  -- Visual 模式
+  opleader = {
+    line = "gc",
+    bock = "gb",
+  },
+}
+-- ctrl + /
+map("n", "<C-_>", "gcc", { noremap = false })
+map("v", "<C-_>", "gcc", { noremap = false })
+
+
 -- bufferline
 -- 左右Tab切换
 map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
